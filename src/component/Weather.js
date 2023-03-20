bc358c1d70e77159cc45c017ee0db41685f3ca85
Sumit <sumitgupta19900@gmail.com>
2023-03-20 23:48:42 +0530

    20-3

--- src/component/Weather.js
+++ src/component/Weather.js
@@ -6,30 +6,34 @@ export default class Weather extends Component {
   constructor() {
     super();
     this.state = {
-      reports: []
+      reports: [],
+      resp: {}
     }
   }
   async componentDidMount() {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=lucknow&lang=en&appid=ba557d896ef43bc68215bc716d689923&units=metric`
     let data = await fetch(url); let parseData = await data.json();
-    this.setState({ reports: parseData.weather })
+    this.setState({ reports: parseData.weather, resp: parseData })
+    console.log(this.state.resp);
 
   }
 
   render() {
     return (
-      <div className='container  my-4'>
-        <h3>Weather is..</h3>
-        <div className="row card" >
-          {this.state.reports.map((element) => {
-            return <div className="my-3 " key={element.id} >
-              {/* <WeatherItem Region={element.name} latitude={element.coord.lat} longitude={element.coord.lon} temp={element.main.temp} min_temp={element.main.temp_min} max_temp={element.main.temp_max} /> */}
-              <WeatherItem Region={element.id} />
-            </div>
-          })}
+      <>
+        <div className='container  my-4'>
+          <h3>Weather is..</h3>
+          <div className="row card" >
+            {this.state.reports.map((element) => {
+              return <div className="my-3 " key={element.id} >
+                <WeatherItem name={this.state.resp.name}Region={this.state.resp.id} latitude={this.state.resp.coord.lat} longitude={this.state.resp.coord.lon} temp={this.state.resp.main.temp} min_temp={this.state.resp.main.temp_min} max_temp={this.state.resp.main.temp_max} humid={this.state.resp.main.humidity} press={this.state.resp.main.pressure} desc={this.state.resp.weather[0].main}/>
+                {/* <WeatherItem Region={element.name} latitude={element.coord.lat} longitude={element.coord.lon} temp={element.main.temp} min_temp={element.main.temp_min} max_temp={element.main.temp_max} /> */}
+              </div>
+            })}
 
+          </div>
         </div>
-      </div>
+      </>
     )
   }
 }
--- src/component/WeatherItem.js
+++ src/component/WeatherItem.js
@@ -4,13 +4,12 @@ export default class WeatherItem extends Component {
 
 
     render() {
-        let { Region, latitude, longitude, temp, min_temp, max_temp } = this.props;
+        let { Region, latitude, longitude, temp, humid, press, name, min_temp, max_temp, desc } = this.props;
         return (
             <div className='container my-3'>
                 <div>
-                    <p className="card-title">{Region}..</p>
-                    <p className="card-text">Weather of location {latitude}, {longitude} at  is {temp} and min temp is {min_temp} and max temp is {max_temp} </p>
-
+                    <p className="card-title ">{name} - {desc} today</p>
+                    <p className="card-text">Temperature of "{name}" (Latitued ={latitude}, Longitude={longitude}) is {temp}( min temp is {min_temp} and max temp is {max_temp}), Pressure is {press} and Humidity is {humid}.</p>
                 </div>
             </div>
 
