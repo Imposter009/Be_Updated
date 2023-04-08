5b94435c9d3aad3cf7d05117fd10b19af0dc3a93
Sumit <sumitgupta19900@gmail.com>
2023-04-09 01:57:43 +0530

    finally

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route, Link
+} from 'react-router-dom';
+
 import NavBar from './component/NavBar';
 import News from './component/News';
 import Weather from './component/Weather';
@@ -9,11 +15,21 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        {/* <Weather /> */}
+        <Router>
         <NavBar />
-        <Weather />
-        <News key='1' size={9} category='general' />
-        <News key='2' size={9} category='sport' />
-        <News key='3' size={9} category='education' />
+          <Routes>
+            <Route exact path="/weather" element={<Weather/>} />
+            <Route exact path="/general" element={<News key='general' size={9} category='general' />} />
+            <Route exact path="/sports" element={<News key='sports' size={9} category='sports' />} />
+            <Route exact path="/technology" element={<News key='technology' size={9} category='technology' />} />
+            <Route exact path="/science" element={<News key='science' size={9} category='science' />} />
+            <Route exact path="/health" element={<News key='health' size={9} category='health' />} />
+            <Route exact path="/education" element={<News key='education' size={9} category='education' />} />
+            <Route exact path="/entertainment" element={<News key='entertainment' size={9} category='entertainment' />} />
+          </Routes>
+        </Router>
+
       </div>
     )
   }
--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {Link } from "react-router-dom";
 
 export default class NavBar extends Component {
     render() {
@@ -6,29 +7,32 @@ export default class NavBar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg bg-body-tertiary">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="/">Updates</a>
+                        <Link className="navbar-brand" to="/">Updates</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
                                 <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                    <Link className="nav-link active" aria-current="page" to="/general">Home </Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link disabled">Weather</a>
+                                    <Link className="nav-link active" to='/sports'>Sports </Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link disabled">Headlines</a>
+                                    <Link className="nav-link active" to='/science'>Science </Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link disabled">Sports-News</a>
+                                    <Link className="nav-link active" to='/technology'>Technology </Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link disabled">Business-News</a>
+                                    <Link className="nav-link active" to='/health'>Health </Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link disabled">Market-News</a>
+                                    <Link className="nav-link active" to='/entertainment'>Entertainment </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link active" to='/weather' >Weather</Link>
                                 </li>
                             </ul>
                             <form className="d-flex" role="search">
